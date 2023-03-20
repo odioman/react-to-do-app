f25daf6329375d3423cab28f39b68e76beaaf9e2
Thomas Koshy <ttkoshy@gmail.com>
2023-03-20 18:59:32 -0400

    add and delete functionality added

--- src/App.js
+++ src/App.js
@@ -1,73 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Todo from './components/Todo';
+import Form from './components/Form';
+import FilterButton from './components/FilterButton';
+import { nanoid } from 'nanoid';
 
 function App(props) {
+  const [tasks, setTasks] = useState(props.tasks)
+
+  function addTask(name) {
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      if (id === task.id) {
+        return {...task, completed: !task.completed}
+      }
+      return task
+    });
+    setTasks(updatedTasks);   
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  const taskList = tasks.map((task) => (
+  <Todo 
+    id={task.id} 
+    name={task.name} 
+    completed={task.completed} 
+    key={task.id}
+    toggleTaskCompleted={toggleTaskCompleted}
+    deleteTask={deleteTask}
+  />
+  ));
+
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+  
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What do you have to do?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add  
-        </button>
-      </form>
+      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show</span>
-          <span>All</span>
-          <span className="visually-hidden">tasks</span>  
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show</span>
-          <span>Active</span>
-          <span className="visually-hidden">tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show</span>
-          <span>Completed</span>
-          <span className="visually-hidden">tasks</span>
-        </button>
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading">
-          <li className="todo stack-small">
-            <div className="c-cb">
-              <input id="todo-0" type="checkbox" defaultChecked={true} />
-              <label className="todo-label" htmlFor="todo-0">
-                Eat
-              </label>
-            </div>
-            <div className="btn-group">
-              <button type="button" className="btn">
-                Edit<span className="visually-hidden">Eat</span>
-              </button>
-              <button type="button" className="btn btn__danger">
-                Delete <span classname="visually-hidden">Eat</span>
-              </button>
-            </div>
-          </li>
-          <li className="todo stack-small">
-            <div className="c-cb">
-              <input id="todo-1" type="checkbox" />
-              <label className="todo-label" htmlFor="todo-1">Sleep</label>
-            </div>
-            <div className="btn-group">
-              <button type="button" className="btn"></button>
-            </div>
-          </li>
+         {taskList}
         </ul>
     </div>
   )
